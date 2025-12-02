2508adf8731d9f6c829775f1902950b3bc09ad12
Declan McGrellis <declanmcgrellis3@gmail.com>
2025-12-01 21:17:13 -0500

    small testing

--- ESC_Manager.py
+++ ESC_Manager.py
@@ -6,13 +6,15 @@ class ESC_Manager:
     def __init__(self, escs = list[ESC]):
         self.pg = pigpio.pi()
         self.escs = escs
-
+        self.wid = -1
+        '''
         for i in escs:
             self.pg.set_mode(i.pin, pigpio.OUTPUT)
             self.pg.write(i.pin, 0)
+        '''
 
     def build_wave(self):
-        self.pg.wave_clear()
+        #self.pg.wave_clear()
 
         on_times = []
         pins = []
@@ -24,6 +26,7 @@ class ESC_Manager:
         on_times = [o for o, p in pair]
         pins = [p for o, p in pair]
         
+        print(f"Pins: {pins}, On Times: {on_times}      ")
         wave = []
         # first section of all waveforms: all gpio pins we are controlling turn on
         gpiomask = 0
@@ -32,22 +35,29 @@ class ESC_Manager:
         wave.append(pigpio.pulse(0, gpiomask, on_times[0]))
 
         #iteratively calculate how long to wait to turn off each pin
-        gpiomask = 1 << pins[0]
+        gpiomask = (1 << pins[0])
+        print(gpiomask)
         for i in range(0, len(on_times)):
             # Checks if gpio pin is already in the bitmask. 
             # If it is, we can assume it has been set properly and can continue the loop
             if (gpiomask & (1 << pins[i])) == gpiomask: continue
-            else: gpiomask |= pins[i]
+            else: gpiomask |= (1 << pins[i])
             for j in range(i+1, len(on_times)):
                 if on_times[j] == on_times[i]: 
                     gpiomask |= (1 << on_times[j])
                 else:
                     wave.append(pigpio.pulse(gpiomask, 0, on_times[j] - on_times[i]))
                     break
+        print(gpiomask)
         wave.append(pigpio.pulse(0, 0, ESC.FRAME_TIME - 2*on_times[-1]))
-
+        print(wave)
         self.pg.wave_add_generic(wave)
-        self.wid = self.pg.wave_create_and_pad(50)
+        self.wid = self.pg.wave_create()
+        info = self.pg.wave_get_cbs()
+        print(info)
+        print(self.pg.wave_get_pulses())     # shows timing and bitmasks of each pulse
+        print(self.pg.wave_get_max_pulses())
+
                 
     def UpdateESCS(self, throttles: list[float]):
         for i in range(0, len(throttles)):
--- testPIG.py
+++ testPIG.py
@@ -16,8 +16,9 @@ BR = ESC(27)
 BL = ESC(22)
 
 manager = ESC_Manager([FL, FR, BR, BL])
+manager.UpdateESCS([0, 0, 0, 0])
 
-manager.UpdateESCS([0.0, 0.0, 0.0, 0.0])
+#manager.UpdateESCS([0, 1, 0, 0])
 
 while(True):
     if joy.buttons[Controller.FW_BK] < 0: 
@@ -29,3 +30,4 @@ while(True):
     else: 
         manager.UpdateESCS([0.0, 0.0, 0.0, 0.0])
     print(f"FR: {FR.getThrottle():.2f} FL: {FL.getThrottle():.2f} BR: {BR.getThrottle():.2f} BL: {BL.getThrottle():.2f}      ", end="\r")
+    print("\033[F\r\033[F\r\033[F\r\033[F\r\033[F\r")
